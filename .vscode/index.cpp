4e169dcd644926e757bfb263bef31ce288d9c55e
Anshika02006 <anshikaagrawa2006@gmail.com>
2026-06-26 16:02:10 +0530

    initial commit

---
+++ .vscode/index.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+// int main()
+// {
+// int a,b,c;
+// cout<<"enter two numbers";
+// cin>>a>>b;
+// c=a%b;
+// cout<<"ans="<<c;
+// return 0;
+// }
+int main(){
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"even";
+    }
+    else
+    {
+        cout<<"odd";
+    }
+    return 0;
+}
---
+++ app.js
@@ -0,0 +1,5 @@
+ //let a=9;
+ const b;
+// var c=6;
+ const b=5;
+ console.log(b);
